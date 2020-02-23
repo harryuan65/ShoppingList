6caba7e036bc40f082f01b68a2a76210c58f6331
Harry Yuan <harryuan.65@gmail.com>
2020-02-23 20:05:52 +0800

    shoppinglist

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Bread',
+    },
+    {
+      id: uuid(),
+      text: 'Steak',
+    },
+    {
+      id: uuid(),
+      text: 'Juice',
+    },
+    {
+      id: uuid(),
+      text: 'Candy',
+    },
+  ]);
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('錯誤', '不要輸入空白姆', {text: 'OK'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header title="ShoppingList" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
